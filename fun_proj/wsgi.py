6b74007f5c067959d15851fb592f267fa2d08ad6
mohammad <moghaddam22@gmail.com>
2016-05-11 17:19:39 -0700

    added jango_setting_mudle envar to wsgi

--- fun_proj/wsgi.py
+++ fun_proj/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fun_proj.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
