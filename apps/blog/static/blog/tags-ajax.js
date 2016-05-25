6f93e8b639df99d7771febd43882974fda632067
mohammad <moghaddam22@gmail.com>
2016-05-25 11:24:40 -0700

    some more work on ajax_tags

--- apps/blog/static/blog/tags-ajax.js
+++ apps/blog/static/blog/tags-ajax.js
@@ -3,18 +3,17 @@
     var url = $('#tags-url', tag_position).attr('href');
     //console.log(url);
     $.getJSON(url, function(data){
-        var items = [];
         $.each(data, function(key, val){
-            var label = $('<span ><a></a></span> ')
+            var label = $('<span><a></a></span> ')
                         .attr('style', 'font-size:1em')
-                        .attr('line-height', '200%')
+                        .attr('line-height', '300%')
                         .addClass('label label-info')
                         .find('a')
                         .attr('href', val)
                         .text(key)
                         .end()
                         .appendTo(tag_position);
-            var dummy = $('<p></p>').appendTo(tag_position); 
+            var dummy = $('<span> </span>').appendTo(tag_position); 
         });
     });
 })(jQuery);
