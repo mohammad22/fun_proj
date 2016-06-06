ce7c805a13dff0c3a596385762d5003732a94119
mohammad <moghaddam22@gmail.com>
2016-06-05 17:49:09 -0700

    added redis cashe. functionality

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,12 +2,27 @@ from django.views import generic
 from django.shortcuts import render_to_response
 from . import models
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+
+class cacheMixin(object):
+    cache_timeout =  60 * 60 * 24  
+
+    def get_cache_timeout(self):
+        return self.cache_timeout
+       
+    def dispatch(self, *args, **kwargs):
+        dis = super(cacheMixin, self).dispatch
+        if self.request.user.is_staff:
+            return dis(*args, **kwargs)
+        else:
+            return cache_page(self.get_cache_timeout())(dis)(*args, **kwargs)
 
 
+@cache_page(60 * 60 * 24)
 def about(request):
     return render_to_response('blog/about.html')
 
-class HomePageView(generic.ListView):
+class HomePageView(cacheMixin, generic.ListView):
     model = models.Post
     ordering = '-date'
     paginate_by = 3
@@ -19,13 +34,14 @@ class HomePageView(generic.ListView):
         else:
             return li.filter(published=True)
 
-class PostView(generic.DetailView):
+class PostView(cacheMixin, generic.DetailView):
     model = models.Post
     def get_object(self, *args, **kwargs):
         obj = super(PostView, self).get_object(*args, **kwargs)
         obj.images = models.Image.objects.filter(post = obj)
         return obj
 
+@cache_page(60 * 60 * 24)
 def TagsView(request):
     tags = models.Tag.objects.all()
     pre_tags = {}
@@ -33,7 +49,7 @@ def TagsView(request):
     return JsonResponse(pre_tags, safe = False)
 
 
-class TagDetailView(generic.DetailView):
+class TagDetailView(cacheMixin, generic.DetailView):
     model = models.Tag
     def get_object(self, *args, **kwargs):
         obj = super(TagDetailView, self).get_object(*args, **kwargs)
